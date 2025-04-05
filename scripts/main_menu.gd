57c04a352ea95704b5c58d993632c4d1cf1c51e4
Lukas Rasocha <rasocha.lukas@gmail.com>
2025-04-05 11:46:27 +0200

    main menu connected to lukas's game

---
+++ scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	print("On start pressed")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_how_to_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_about_pressed() -> void:
+	pass # Replace with function body.
